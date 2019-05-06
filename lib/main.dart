39c97b042e12f706d387a57c3658507dffb16ab9
Enzo Lizama <lizama.enzo@gmail.com>
2019-05-06 18:10:36 -0500

    update main to run the home page as main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:buscatelo/ui/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,11 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'BuscaTelo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: Container(),
+      home: HomePage(),
     );
   }
 }
