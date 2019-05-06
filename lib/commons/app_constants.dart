84c84b45cae6fc2b1f30146f071df6cba780527d
Enzo Lizama <lizama.enzo@gmail.com>
2019-05-06 18:11:43 -0500

    app constants for the app

---
+++ lib/commons/app_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static final Color tabBackgroundColor = Color(0xFFe2d7f5);
+  static final Color tabActiveItemColor = Color(0xFFe2d7f5);
+  static final double radiusBorderValue = 20.0;
+  static final double activeTabIconSize = 30.0;
+  static final double activeTabTextSize = 18.0;
+}
