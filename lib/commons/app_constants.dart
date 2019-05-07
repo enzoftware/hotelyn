3c76f65593bb5f1557f7ebdf396d777a29a71fe5
Enzo Lizama <lizama.enzo@gmail.com>
2019-05-07 09:58:50 -0500

    adding colors to constants

--- lib/commons/app_constants.dart
+++ lib/commons/app_constants.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppConstants {
-  static final Color tabBackgroundColor = Color(0xFFe2d7f5);
-  static final Color tabActiveItemColor = Color(0xFFe2d7f5);
+  static final Color backgroundColor = Color(0xFFe2d7f5);
+  static final Color primaryColor = Color(0xff6732c1);
+  static final Color accentColor = Color(0xfffd8c00);
   static final double radiusBorderValue = 20.0;
   static final double activeTabIconSize = 30.0;
   static final double activeTabTextSize = 18.0;
+  static final String avatarImage = "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb";
 }
