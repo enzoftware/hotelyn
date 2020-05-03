d251a5653ae7337ac10233ba47a19bebfba55943
Enzo Lizama <lizama.enzo@gmail.com>
2020-05-02 23:29:32 -0500

    :robot: add state managment

---
+++ lib/bloc/hotel_bloc.dart
@@ -0,0 +1,19 @@
+import 'package:buscatelo/data/repository/hotel_repository.dart';
+import 'package:buscatelo/data/repository/mock_hotel_repository.dart';
+import 'package:buscatelo/model/hotel_model.dart';
+import 'package:flutter/material.dart';
+
+class HotelBloc extends ChangeNotifier {
+  HotelRepository repository = MockHotelRepository();
+
+  /// Private list of [HotelModel]
+  List<HotelModel> _hotels;
+
+  /// Public getter for hotels
+  List<HotelModel> get hotels => _hotels;
+
+  void retrieveHotels() async {
+    _hotels = await repository.fetchHotels();
+    notifyListeners();
+  }
+}
