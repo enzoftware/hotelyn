68913dad9e19c60c8cf5a869cff3a580918f97ca
Enzo Lizama <lizama.enzo@gmail.com>
2020-05-03 23:25:04 -0500

    add gesture detector to go to detail screen

--- lib/ui/pages/hotel_search/hotel_item.dart
+++ lib/ui/pages/hotel_search/hotel_item.dart
@@ -1,5 +1,6 @@
 import 'package:buscatelo/commons/theme.dart';
 import 'package:buscatelo/model/hotel_model.dart';
+import 'package:buscatelo/ui/pages/hotel_detail/hotel_detail_page.dart';
 import 'package:flutter/material.dart';
 
 class HotelItem extends StatelessWidget {
@@ -9,77 +10,81 @@ class HotelItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: Image.network(hotel.imageUrl),
+    return GestureDetector(
+      onTap: () => HotelDetailPage.navigate(hotel),
+      child: Container(
+        child: Stack(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(hotel.imageUrl),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(40.0),
-            child: Card(
-              elevation: 8,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0)),
-              margin: const EdgeInsets.only(top: 200),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            color: primaryColor,
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              'FOR RENT',
-                              style: TextStyle(color: Colors.white),
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Card(
+                elevation: 8,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+                margin: const EdgeInsets.only(top: 200),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              color: primaryColor,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'FOR RENT',
+                                style: TextStyle(color: Colors.white),
+                              ),
                             ),
                           ),
                         ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'S/ ${hotel.price.toString()}',
+                            style: priceTextStyle,
+                          ),
+                        ),
+                      ],
+                    ),
+                    ListTile(
+                      title: Text(
+                        hotel.name,
+                        style: titleTextStyle,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'S/ ${hotel.price.toString()}',
-                          style: priceTextStyle,
+                      subtitle: Text(hotel.address),
+                      trailing: Container(
+                        decoration: BoxDecoration(
+                            color: accentColor, shape: BoxShape.circle),
+                        child: Transform.rotate(
+                          angle: 25 * 3.1416 / 180,
+                          child: IconButton(
+                            icon: Icon(Icons.navigation),
+                            onPressed: () {},
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ],
-                  ),
-                  ListTile(
-                    title: Text(
-                      hotel.name,
-                      style: titleTextStyle,
-                    ),
-                    subtitle: Text(hotel.address),
-                    trailing: Container(
-                      decoration: BoxDecoration(
-                          color: accentColor, shape: BoxShape.circle),
-                      child: Transform.rotate(
-                        angle: 25 * 3.1416 / 180,
-                        child: IconButton(
-                          icon: Icon(Icons.navigation),
-                          onPressed: () {},
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
