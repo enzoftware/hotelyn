d1598f91209aa9653e4f16291f72bfb6a89c1c28
Enzo Lizama <lizama.enzo@gmail.com>
2020-05-03 23:44:35 -0500

    change repositoty impl to remote

--- lib/bloc/hotel_bloc.dart
+++ lib/bloc/hotel_bloc.dart
@@ -1,10 +1,10 @@
 import 'package:buscatelo/data/repository/hotel_repository.dart';
-import 'package:buscatelo/data/repository/mock_hotel_repository.dart';
+import 'package:buscatelo/data/repository/remote_hotel_repository.dart';
 import 'package:buscatelo/model/hotel_model.dart';
 import 'package:flutter/material.dart';
 
 class HotelBloc extends ChangeNotifier {
-  HotelRepository repository = MockHotelRepository();
+  HotelRepository repository = RemoteHotelRepository();
 
   /// Private list of [HotelModel]
   List<HotelModel> _hotels;
