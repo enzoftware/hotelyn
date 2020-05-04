e99ceb9c848e9d82c957ff65fc3fe0692ee644e7
Enzo Lizama <lizama.enzo@gmail.com>
2020-05-03 23:24:28 -0500

    fix hotel model for server

--- lib/model/hotel_model.dart
+++ lib/model/hotel_model.dart
@@ -1,52 +1,124 @@
 class HotelModel {
-  int id;
-  String address;
-  String phone;
-  String description;
-  String email;
-  String rate;
   String name;
+  String address;
   String imageUrl;
-  double price;
+  int price;
+  List<Rooms> rooms;
+  List<Reviews> reviews;
   List<Amenities> amenities;
 
   HotelModel({
-    this.id,
-    this.address,
-    this.phone,
-    this.description,
-    this.email,
-    this.rate,
     this.name,
+    this.address,
     this.imageUrl,
-    this.price,
-    // this.amenities,
+    this.rooms,
+    this.reviews,
+    this.amenities,
   });
 
   HotelModel.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
+    name = json['name'];
+    price = json['price'];
+    imageUrl = json['imageUrl'];
     address = json['address'];
-    phone = json['phone'];
-    description = json['description'];
-    email = json['email'];
-    rate = json['rate'];
+    if (json['rooms'] != null) {
+      rooms = List<Rooms>();
+      json['rooms'].forEach((v) {
+        rooms.add(Rooms.fromJson(v));
+      });
+    }
+    if (json['reviews'] != null) {
+      reviews = List<Reviews>();
+      json['reviews'].forEach((v) {
+        reviews.add(Reviews.fromJson(v));
+      });
+    }
+    if (json['amenities'] != null) {
+      amenities = List<Amenities>();
+      json['amenities'].forEach((v) {
+        amenities.add(Amenities.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['name'] = this.name;
+    data['price'] = this.price;
+    data['address'] = this.address;
+    data['imageUrl'] = this.imageUrl;
+    if (this.rooms != null) {
+      data['rooms'] = this.rooms.map((v) => v.toJson()).toList();
+    }
+    if (this.reviews != null) {
+      data['reviews'] = this.reviews.map((v) => v.toJson()).toList();
+    }
+    if (this.amenities != null) {
+      data['amenities'] = this.amenities.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Rooms {
+  String imageUrl;
+  String name;
+
+  Rooms({this.imageUrl, this.name});
+
+  Rooms.fromJson(Map<String, dynamic> json) {
+    imageUrl = json['imageUrl'];
     name = json['name'];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['address'] = this.address;
-    data['phone'] = this.phone;
-    data['description'] = this.description;
-    data['email'] = this.email;
-    data['rate'] = this.rate;
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['imageUrl'] = this.imageUrl;
     data['name'] = this.name;
     return data;
   }
 }
 
+class Reviews {
+  String message;
+  String user;
+  String userImage;
+  int rate;
+
+  Reviews({this.message, this.user, this.userImage, this.rate});
+
+  Reviews.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    user = json['user'];
+    userImage = json['userImage'];
+    rate = json['rate'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['message'] = this.message;
+    data['user'] = this.user;
+    data['userImage'] = this.userImage;
+    data['rate'] = this.rate;
+    return data;
+  }
+}
+
 class Amenities {
-  String title;
-  String iconUrl;
+  String name;
+  String imageUrl;
+
+  Amenities({this.name, this.imageUrl});
+
+  Amenities.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    imageUrl = json['imageUrl'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['name'] = this.name;
+    data['imageUrl'] = this.imageUrl;
+    return data;
+  }
 }
