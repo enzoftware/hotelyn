a663c2f2c1ad3a030ce6e91af6d9f5f69f2d3065
Enzo Lizama <lizama.enzo@gmail.com>
2020-05-16 02:18:23 -0500

    update hotel rooms tab

--- lib/ui/pages/hotel_detail/room/hotel_room_tab.dart
+++ lib/ui/pages/hotel_detail/room/hotel_room_tab.dart
@@ -1,12 +1,19 @@
+import 'package:buscatelo/model/hotel_model.dart';
+import 'package:buscatelo/ui/pages/hotel_detail/room/room_item.dart';
 import 'package:flutter/material.dart';
 
 class HotelRoomTab extends StatelessWidget {
+  final List<Room> rooms;
   const HotelRoomTab({
     Key key,
+    this.rooms,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text("room");
+    return ListView.builder(
+      itemCount: rooms.length,
+      itemBuilder: (context, index) => RoomItem(room: rooms[index]),
+    );
   }
 }
--- lib/ui/pages/hotel_detail/room/room_item.dart
+++ lib/ui/pages/hotel_detail/room/room_item.dart
@@ -1,8 +1,45 @@
+import 'package:bordered_text/bordered_text.dart';
+import 'package:buscatelo/model/hotel_model.dart';
 import 'package:flutter/material.dart';
 
 class RoomItem extends StatelessWidget {
+  final Room room;
+
+  const RoomItem({Key key, this.room}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      semanticContainer: true,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Image.network(
+            room.imageUrl,
+            fit: BoxFit.fill,
+          ),
+          Center(
+            child: BorderedText(
+              strokeWidth: 4.0,
+              strokeColor: Colors.black,
+              child: Text(
+                room.name,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 21.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      elevation: 5,
+      margin: EdgeInsets.all(8),
+    );
   }
 }
