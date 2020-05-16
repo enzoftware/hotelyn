3b7f245856cf0a4b4de25cffe9148f50438be35a
Enzo Lizama <lizama.enzo@gmail.com>
2020-05-16 02:18:12 -0500

    update hotel review tab

--- lib/ui/pages/hotel_detail/review/hotel_review_tab.dart
+++ lib/ui/pages/hotel_detail/review/hotel_review_tab.dart
@@ -1,12 +1,22 @@
+import 'package:buscatelo/model/hotel_model.dart';
+import 'package:buscatelo/ui/pages/hotel_detail/review/review_item.dart';
 import 'package:flutter/material.dart';
 
 class HotelReviewTab extends StatelessWidget {
+  final List<Review> reviews;
   const HotelReviewTab({
     Key key,
+    this.reviews,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Icon(Icons.directions_bike);
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: ListView.builder(
+        itemCount: reviews.length,
+        itemBuilder: (context, index) => ReviewItem(review: reviews[index]),
+      ),
+    );
   }
 }
--- lib/ui/pages/hotel_detail/review/review_item.dart
+++ lib/ui/pages/hotel_detail/review/review_item.dart
@@ -1,8 +1,22 @@
+import 'package:buscatelo/model/hotel_model.dart';
 import 'package:flutter/material.dart';
 
 class ReviewItem extends StatelessWidget {
+  final Review review;
+  const ReviewItem({
+    Key key,
+    this.review,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      child: ListTile(
+        leading: Image.network(review.userImage),
+        title: Text(review.message),
+        subtitle: Text(review.user),
+        trailing: Text(review.rate.toString()),
+      ),
+    );
   }
 }
