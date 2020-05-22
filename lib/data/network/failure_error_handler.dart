12baff1f091da42e73865a3673a96573d824d3c2
Enzo Lizama <lizama.enzo@gmail.com>
2020-05-22 00:03:12 -0500

    error handler for network requests

---
+++ lib/data/network/failure_error_handler.dart
@@ -0,0 +1,9 @@
+class Failure {
+  final String message;
+  final int statusCode;
+
+  Failure(this.message, this.statusCode);
+
+  @override
+  String toString() => "Error $statusCode. $message.";
+}
--- lib/data/network/hotel_api.dart
+++ lib/data/network/hotel_api.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
+import 'package:buscatelo/data/network/failure_error_handler.dart';
 import 'package:buscatelo/model/hotel_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -13,8 +15,14 @@ class HotelApi {
       final data = await http.get(_baseUrl + _endPoint);
       final responseList = json.decode(data.body);
       return [for (final hotel in responseList) HotelModel.fromJson(hotel)];
+    } on SocketException {
+      throw Failure("No internet connection", 400);
+    } on HttpException {
+      throw Failure("Not found request", 404);
+    } on FormatException {
+      throw Failure("Invalid JSON format", 666);
     } catch (e) {
-      throw Exception(e);
+      throw Failure("Unknown error", 888);
     }
   }
 }
