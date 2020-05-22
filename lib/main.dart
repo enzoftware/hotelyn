7b970a3ba9347584b7d8972633d0261f80c721b3
Enzo Lizama <lizama.enzo@gmail.com>
2020-05-22 11:49:10 -0500

    update custom error widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,13 @@
 import 'package:buscatelo/bloc/hotel_bloc.dart';
 import 'package:buscatelo/commons/theme.dart';
 import 'package:buscatelo/ui/pages/hotel_search/home_page.dart';
+import 'package:buscatelo/ui/utils/error_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  ErrorWidget.builder = (FlutterErrorDetails details) => LifeErrorWidget();
+  ErrorWidget.builder = (FlutterErrorDetails details) => CustomErrorWidget();
   runApp(MyApp());
-}
-
-class LifeErrorWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Image.network(
-          "https://i.pinimg.com/originals/3f/87/c5/3f87c5b2a2e06bfaf5d37d974f607a02.jpg",
-          width: 200,
-          height: 200,
-        ),
-        Text(
-          "Ocurrio un error :(",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ],
-    ));
-  }
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/ui/utils/error_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CustomErrorWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Image.network(
+          "https://i.pinimg.com/originals/3f/87/c5/3f87c5b2a2e06bfaf5d37d974f607a02.jpg",
+          width: 200,
+          height: 200,
+        ),
+        Text(
+          "Ocurrio un error :(",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    ));
+  }
+}
