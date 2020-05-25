d08e398966e3faf3c5689374c7e19985ce1016a1
Enzo Lizama <lizama.enzo@gmail.com>
2020-05-25 00:30:37 -0500

    improving error handler in repository layer

--- lib/data/network/hotel_api.dart
+++ lib/data/network/hotel_api.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
-import 'package:buscatelo/data/network/failure_error_handler.dart';
 import 'package:buscatelo/model/hotel_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,18 +9,8 @@ class HotelApi {
       "/enzoftware/hotel_booking_app/master/server/hotels.json";
 
   Future<List<HotelModel>> getHotels() async {
-    try {
-      final data = await http.get(_baseUrl + _endPoint);
-      final responseList = json.decode(data.body);
-      return [for (final hotel in responseList) HotelModel.fromJson(hotel)];
-    } on SocketException {
-      throw Failure("No internet connection", 400);
-    } on HttpException {
-      throw Failure("Not found request", 404);
-    } on FormatException {
-      throw Failure("Invalid JSON format", 666);
-    } catch (e) {
-      throw Failure("Unknown error", 888);
-    }
+    final data = await http.get(_baseUrl + _endPoint);
+    final responseList = json.decode(data.body);
+    return [for (final hotel in responseList) HotelModel.fromJson(hotel)];
   }
 }
--- lib/data/repository/remote_hotel_repository.dart
+++ lib/data/repository/remote_hotel_repository.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:buscatelo/data/network/failure_error_handler.dart';
 import 'package:buscatelo/data/network/hotel_api.dart';
 import 'package:buscatelo/data/repository/hotel_repository.dart';
 import 'package:buscatelo/model/hotel_model.dart';
@@ -5,7 +8,17 @@ import 'package:buscatelo/model/hotel_model.dart';
 class RemoteHotelRepository extends HotelRepository {
   @override
   Future<List<HotelModel>> fetchHotels() async {
-    final api = HotelApi();
-    return await api.getHotels();
+    try {
+      final api = HotelApi();
+      return await api.getHotels();
+    } on SocketException {
+      throw Failure("No internet connection", 400);
+    } on HttpException {
+      throw Failure("Not found request", 404);
+    } on FormatException {
+      throw Failure("Invalid JSON format", 666);
+    } catch (e) {
+      throw Failure("Unknown error", 888);
+    }
   }
 }
