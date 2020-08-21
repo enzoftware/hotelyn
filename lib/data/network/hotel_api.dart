b076cd862a9a31a91b5ed1714192e32f0b4ecef0
Enzo Lizama <lizama.enzo@gmail.com>
2020-08-21 17:13:50 -0500

    adding http request testing

--- lib/data/network/hotel_api.dart
+++ lib/data/network/hotel_api.dart
@@ -2,14 +2,17 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:buscatelo/model/hotel_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 class HotelApi {
   final String _baseUrl = 'https://raw.githubusercontent.com';
   final String _endPoint =
       '/enzoftware/hotel_booking_app/master/server/hotels.json';
 
+  Client client = http.Client();
+
   Future<List<HotelModel>> getHotels() async {
-    final data = await http.get(_baseUrl + _endPoint);
+    final data = await client.get(_baseUrl + _endPoint);
     final responseList = json.decode(data.body);
     return [for (final hotel in responseList) HotelModel.fromJson(hotel)];
   }
--- test/data/hotel_repository_test.dart
+++ test/data/hotel_repository_test.dart
@@ -1,1 +1,27 @@
-void main() {}
+import 'dart:convert';
+
+import 'package:buscatelo/data/network/hotel_api.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+void main() {
+  test('Testing the network call', () async {
+    final apiProvider = HotelApi();
+    apiProvider.client = MockClient((request) async {
+      var mockJsonResponse = [
+        {'name': 'hotel A', 'price': 100},
+        {'name': 'hotel B', 'price': 200},
+        {'name': 'hotel C', 'price': 300},
+      ];
+      return Response(json.encode(mockJsonResponse), 200);
+    });
+    var hotels = await apiProvider.getHotels();
+    expect(hotels.length, 3);
+
+    for (var i = 0; i < hotels.length; i++) {
+      assert(hotels[i].name != null);
+    }
+  });
+}
