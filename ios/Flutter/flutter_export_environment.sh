5aa33a236999a057c84148f8246f3e13ed9b2785
Enzo Lizama <lizama.enzo@gmail.com>
2023-07-17 22:23:41 -0500

    Update Flutter dependencies

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/enzolizama/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/enzolizama/Projects/hotelyn/hotelyn"
+export "FLUTTER_APPLICATION_PATH=/Users/enzolizama/Projects/hotelyn"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
