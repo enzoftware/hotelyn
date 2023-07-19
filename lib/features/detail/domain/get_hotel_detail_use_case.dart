202c1f12e23ebe240aa49c35d3ecbea2302fd365
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2023-07-18 23:44:58 -0500

    ✨  Fresh start for the project (#21)
    * chore: start project
    
    * Update Flutter dependencies
    
    * Add design system package
    
    * chore: update CI/CD version
    
    * chore: update readme
    
    * chore: add test
    
    * chore: update readme
    
    * chore: update cicd
    
    * chore: update flutter version

--- lib/features/detail/domain/get_hotel_detail_use_case.dart
+++
@@ -1,8 +0,0 @@
-class GetHotelDetailUseCase {
-  GetHotelDetailUseCase();
-
-  // Future<Hotel> fetchHotelDetail(String name) async {
-  //   final response = await _hotelRepository.fetchHotelDetail(name);
-  //   // return response.when(success: success, failure: failure);
-  // }
-}
--- lib/features/home/domain/get_hotel_use_case.dart
+++
@@ -1,8 +0,0 @@
-class GetHotelsUseCase {
-  GetHotelsUseCase();
-
-  // Future<List<Hotel>> fetchHotels() async {
-  //   final response = await _hotelRepository.fetchHotels();
-  //   return response;
-  // }
-}
