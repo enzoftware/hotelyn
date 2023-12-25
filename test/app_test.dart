c145f918f61a7afa42733f7c66b437cf99010aa7
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2023-12-25 17:05:29 -0500

    Fix Github actions for tests 🧪  (#45)
    * config: fix github action
    
    * fix: github actions checkout
    
    * chore: bump to v4
    
    * fix test
    
    * test: fix test

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,9 +1,12 @@
+import 'dart:ui';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hotelyn/app.dart';
 
 void main() {
-  setUp(() {});
+  final binding = TestWidgetsFlutterBinding.ensureInitialized();
   testWidgets('App launches successfully', (WidgetTester tester) async {
+    await binding.setSurfaceSize(const Size(600, 800));
     await tester.pumpWidget(const HotelynApp());
   });
 }
