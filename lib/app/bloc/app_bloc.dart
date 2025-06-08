98cbc26e27f7a95873e96100b258d3ea76a67ea0
Enzo Lizama <lizama.enzo@gmail.com>
2025-06-07 22:12:25 -0500

    chore: update dependencies and configuration files
    - Updated `.gitignore` to include new build and Swift package manager directories.
    - Modified `pubspec.lock` to reflect version bumps for several dependencies, including:
      - analyzer: 6.2.0 to 7.4.5
      - async: 2.4.2 to 2.7.0
      - build_runner: 2.4.12 to 2.4.14
      - shared_preferences: 2.3.2 to 2.5.3
      - and others, ensuring compatibility with the latest versions.
    - Removed Firebase dependencies from `pubspec.yaml` as they are no longer needed.
    - Updated iOS project files to support new configurations and frameworks.
    - Refactored `AppBloc` and `UserRepository` to remove Firebase authentication logic.

--- lib/app/bloc/app_bloc.dart
+++ lib/app/bloc/app_bloc.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hotelyn/core/domain/repository/repository.dart';
 import 'package:hotelyn/core/domain/repository/user_repository.dart';
 
@@ -20,9 +19,5 @@ class AppBloc extends Bloc<AppEvent, AppState> {
   FutureOr<void> _onFetchAuthenticationStatus(
     FetchAuthenticationStatus event,
     Emitter<AppState> emit,
-  ) async {
-    _userRepository.user.listen((user) {
-      if (user != null) emit(AppStateAuthenticated(user: user));
-    });
-  }
+  ) async {}
 }
--- lib/core/domain/repository/user_repository.dart
+++ lib/core/domain/repository/user_repository.dart
@@ -1,5 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
 sealed class LoginException implements Exception {}
 
 class WrongPasswordException extends LoginException {}
@@ -7,34 +5,11 @@ class WrongPasswordException extends LoginException {}
 class UserNotFounddException extends LoginException {}
 
 class UserRepository {
-  UserRepository({
-    required FirebaseAuth firebaseAuth,
-  }) : _firebaseAuth = firebaseAuth;
-
-  final FirebaseAuth _firebaseAuth;
-
-  Stream<User?> get user => _firebaseAuth.authStateChanges();
+  UserRepository();
 
   Future<String?> login(String email, String password) async {
-    try {
-      final response = await _firebaseAuth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return response.credential?.accessToken;
-    } on FirebaseAuthException catch (error) {
-      switch (error.code) {
-        case 'invalid-email':
-          throw WrongPasswordException();
-        case 'user-not-found':
-          throw UserNotFounddException();
-        default:
-      }
-    }
     return null;
   }
 
-  Future<void> logout() async {
-    await _firebaseAuth.signOut();
-  }
+  Future<void> logout() async {}
 }
