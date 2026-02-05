eea6bae62feb9d6406162a425d8bdcbb4790eb8f
Enzo Lizama <lizama.enzo@gmail.com>
2026-02-05 01:51:56 -0500

    feat: Implement multi-environment setup for development, staging, and production with distinct app icons and build configurations.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,6 +9,7 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
